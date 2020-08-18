beed1cd908d0e4208af475a0367caebe7864812b
DornalaS <sbdornala@gmail.com>
2020-08-18 13:09:09 +1000

    new test case added by user A

--- src/test/java/mavinautomationlearning/GithubDemo.java
+++ src/test/java/mavinautomationlearning/GithubDemo.java
@@ -2,15 +2,14 @@ package mavinautomationlearning;
 
 import org.testng.annotations.Test;
 
-public class mobileTest {
+public class GithubDemo {
 	
-	
+
 	@Test
 	
 	public void moblogin()
 	{
 		System.out.println("Mobile login sucessfull");
-		System.out.println("Git test casea dded by A");
 	}
 
 	@Test
@@ -20,10 +19,5 @@ public class mobileTest {
 		System.out.println("Mobile error");
 	}
 	
-	
-	@Test
-	public void mobredemption()
-	{
-		System.out.println("Mobile redemption sucessfull");
-	}
+
 }
